48284884b6c2d12a169c02079c25c5b00e351a24
Ramesh Naandi <rameshvai340@gmail.com>
2026-01-03 18:01:43 +0530

    4

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+// app/page.tsx
 import { Hero } from "@/components/hero"
 import { GlobeSection } from "@/components/globe-section"
 import { Button } from "@/components/ui/button"
 import ShopifySection from "@/components/ShopifySection"
 import ShopifyPage from "@/components/ShopifyPage"
 import ForeverCustomers from "@/components/ForeverCustomers"
-import Popup from "@/components/Popup"        // <-- ADD THIS
+import Popup from "@/components/Popup"
 import WikipediaHeroSection from '@/components/WikipediaHeroSection';
 import OurServicesSection from '@/components/OurServicesSection';
 import WikiBenefitsScroll from '@/components/WikiBenefitsSection';
@@ -12,12 +13,11 @@ import WikiServices from "@/components/WikiServices";
 import { WikipediaProcess } from "@/components/WikipediaProcess"
 import { TestimonialsSection } from "@/components/TestimonialsSection"
 import { FaqSection } from "@/components/FaqSection"
-import { Footer } from "@/components/Footer"
+" 
 
 export default function Home() {
   return (
     <div className="flex flex-col relative">
-
       {/* Popup shows after 4 seconds */}
       {/* <Popup /> */}
 
@@ -28,12 +28,12 @@ export default function Home() {
       <WikipediaHeroSection />
       <OurServicesSection />
       {/* <GlobeSection /> */}
-        <WikiBenefitsScroll />
-        {/* <WikiServices /> */}
-         <WikipediaProcess />
-           <TestimonialsSection />
-         <FaqSection />
-         <Footer />
+      <WikiBenefitsScroll />
+      {/* <WikiServices /> */}
+      <WikipediaProcess />
+      <TestimonialsSection />
+      <FaqSection />
+    
     </div>
   )
 }
